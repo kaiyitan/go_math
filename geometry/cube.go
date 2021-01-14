f84f0f52651ff1c353702c3369ea1eb5244ae9d8
Tan Kai Yi <tankaiyi123@gmail.com>
2021-01-14 11:04:17 +0800

    changes'

--- geometry/cube.go
+++ geometry/cube.go
@@ -5,8 +5,6 @@ import "errors"
 func CubeVolume(n int) (int, error) {
 	if n != 0 {
 		return n * n * n, nil
-	} else {
-		return 0, errors.New("Zero length edge is not allowed")
 	}
-
+	return 0, errors.New("Zero length edge is not allowed")
 }
